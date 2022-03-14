8c239c3cc40cd39f211ca9a41c1aa7b29bf6930e
jiyoonZ <juneky416@gmail.com>
2022-03-14 12:54:16 +0900

    20220314 코딩테스트연습

---
+++ 신고하기.js
@@ -0,0 +1,41 @@
+function solution(id_list, report, k) {
+  const tmpList = new Array(id_list.length);
+  tmpList.fill(0);
+  const report_list = {};
+
+  //사용자리스트를 객체의 key값으로 넣기
+  id_list.map((user) => {
+    report_list[user] = [];
+  });
+
+  // 리포트에서 신고한사람을 value로 넣기
+  report.map((user) => {
+    const [user_id, report_id] = user.split(' ');
+    if (!report_list[report_id].includes(user_id)) {
+      report_list[report_id].push(user_id);
+    }
+  });
+
+  for (const key in report_list) {
+    if (report_list[key].length >= k) {
+      //이용정지 유저
+      report_list[key].map((user) => {
+        //신고한 홋수를
+        tmpList[id_list.indexOf(user)] += 1;
+      });
+    }
+  }
+  console.log(tmpList);
+}
+
+const id_list = ['muzi', 'frodo', 'apeach', 'neo'];
+const report = [
+  'muzi frodo',
+  'apeach frodo',
+  'frodo neo',
+  'muzi neo',
+  'apeach muzi',
+];
+const k = 2;
+
+solution(id_list, report, k);
