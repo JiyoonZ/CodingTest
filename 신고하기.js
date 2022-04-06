c72ac8c082cf0ae14fe4f3641d285051ebc05b2f
jiyoonZ <juneky416@gmail.com>
2022-04-06 23:32:43 +0900

    코테

--- 신고하기.js
+++ 신고하기.js
@@ -1,41 +1,54 @@
-function solution(id_list, report, k) {
-  const tmpList = new Array(id_list.length);
-  tmpList.fill(0);
-  const report_list = {};
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-  //사용자리스트를 객체의 key값으로 넣기
-  id_list.map((user) => {
-    report_list[user] = [];
-  });
+rl.on('line', function(line){
+    console.log(line);
+    rl.close(); // 한 줄 입력받고 종료
+}).on('close', function(){
+    process.exit();
+})
 
-  // 리포트에서 신고한사람을 value로 넣기
-  report.map((user) => {
-    const [user_id, report_id] = user.split(' ');
-    if (!report_list[report_id].includes(user_id)) {
-      report_list[report_id].push(user_id);
-    }
-  });
+// function solution(id_list, report, k) {
+//   const tmpList = new Array(id_list.length);
+//   tmpList.fill(0);
+//   const report_list = {};
 
-  for (const key in report_list) {
-    if (report_list[key].length >= k) {
-      //이용정지 유저
-      report_list[key].map((user) => {
-        //신고한 홋수를
-        tmpList[id_list.indexOf(user)] += 1;
-      });
-    }
-  }
-  console.log(tmpList);
-}
+//   //사용자리스트를 객체의 key값으로 넣기
+//   id_list.map((user) => {
+//     report_list[user] = [];
+//   });
 
-const id_list = ['muzi', 'frodo', 'apeach', 'neo'];
-const report = [
-  'muzi frodo',
-  'apeach frodo',
-  'frodo neo',
-  'muzi neo',
-  'apeach muzi',
-];
-const k = 2;
+//   // 리포트에서 신고한사람을 value로 넣기
+//   report.map((user) => {
+//     const [user_id, report_id] = user.split(' ');
+//     if (!report_list[report_id].includes(user_id)) {
+//       report_list[report_id].push(user_id);
+//     }
+//   });
 
-solution(id_list, report, k);
+//   for (const key in report_list) {
+//     if (report_list[key].length >= k) {
+//       //이용정지 유저
+//       report_list[key].map((user) => {
+//         //신고한 홋수를
+//         tmpList[id_list.indexOf(user)] += 1;
+//       });
+//     }
+//   }
+//   console.log(tmpList);
+// }
+
+// const id_list = ['muzi', 'frodo', 'apeach', 'neo'];
+// const report = [
+//   'muzi frodo',
+//   'apeach frodo',
+//   'frodo neo',
+//   'muzi neo',
+//   'apeach muzi',
+// ];
+// const k = 2;
+
+// solution(id_list, report, k);
