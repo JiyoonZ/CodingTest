15a9638d95056f8d1628e31f0032ca3f554a164a
jiyoonZ <juneky416@gmail.com>
2022-04-12 23:53:54 +0900

    0411 lv1 add

--- promgrammers/행렬의덧셈.js
+++ promgrammers/행렬의덧셈.js
@@ -1,5 +1,16 @@
 let arr1 = [[1,2],[2,3]];
 let arr2 = [[3,4],[5,6]];
-
-
-console.log(arr1[0][0].length);
+let format = arr1;
+let result = [];
+let tmp = [];
+let cnt = 0;
+for(let i=0; i<arr1.length; i++) {
+  for(let j=0; j<arr1[i].length; j++) {
+    
+    format[i][j] = arr1[i][j] + arr2[i][j];
+    cnt++;
+    
+  }
+  
+}
+console.log(format);
