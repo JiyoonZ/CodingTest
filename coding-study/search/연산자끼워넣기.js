86507b156df82df3ab0d9caa8be905857a5dffe5
jiyoonZ <juneky416@gmail.com>
2022-05-14 19:27:18 +0900

    Graph DFS&BFS

--- coding-study/search/연산자끼워넣기.js
+++ coding-study/search/연산자끼워넣기.js
@@ -1,18 +1,23 @@
-let input =`4
-3 4 5 6
-1 1 1 0`
-
+let input =`5
+5 4 3 2 1
+1 1 1 1`
+// 문제 주의사항 연산기호의 갯수는 무조건 N -1 이다. 
 // 연산의 순서
 // +, -, * , /
 input = input.split('\n');
+// 수의 갯수
 const N = input[0]
+// 각각의 주어진 숫자
 const nums = input[1].split(' ').map(x=>+x)
+// 연산식의 각각의 갯수
 const operators = input[2].split(' ').map(x=>+x)
 
 function solution(N, nums, operators) {
-
+  // 연산기호의 횟수를 
+  // 연산기호로 횟수만큼 펼쳐놓은 배열을 oper 에 담기
   const oper = changeOper(operators);
-  // console.log(oper);
+  console.log(oper);
+  
   // 방문여부를 체크 => 위에서 chageOper함수를 통해 만든 oper배열의 길이만큼 배열생성 > 모두 false로 채워주기
   const visited = new Array(oper.length).fill(false);
   // console.log(visited);
@@ -20,45 +25,52 @@ function solution(N, nums, operators) {
   let answer = [];
   let results = [];
   dfs(0);
-
+  
+  //정답 출력
   console.log(Math.max(...results));
   console.log(Math.min(...results));
   console.log(results)
   
-  // 4.
-  // 모든 조건을 확인하는 함수
+  
+  // 2.
+  // 깊이우선 완전탐색 DFS로 풀기
   function dfs(cnt) {
-    // cnt가 주어진 연산기호의 갯수가 되었을때 (주어진 숫자보다 연산기호는 1개 적기때문에)
-    // 재귀호출을 반복하면서 모든 연산자가 채워졌을때 계산해주기 
-    // 계산해준값 results 함수에 넣어주기
+    // cnt 
+    // 연산기호를 넣을수 있는 길이는 "주어진 숫자의 개수 -1"
     if(cnt === N-1) {
       results.push(calc(nums,answer));
     }
+	   // depth 마다 근접한 모든 노드를 방문하도록한다.
+		// 깊이마다 재귀호출로 모든 노드 탐색 -> 반복...
 
-    // 연산기호가 들어갈 수 있는 자리의 갯수만큼 탐색하기
+    //===============================
+    //  for 문은 순서대로 노드를 지정
+    // 재귀호출은 자식노드를 모두 탐색으로 보기
+
+    // 연산 기호를 넣을 수 있는 길이만큼 for문 돌리기
+    console.log(answer);
     for(let i=0; i< N-1; i++) {
-      //만약 방문한적있다면 for문 계속진행
-      //첫번째 재귀호출로 이코드 실행되었다면
-      // 0번째 visited는 true 이므로 for문 으로 다시 돌아가기
+      // 방문한적이 있다면 for문 진행
+      // 즉 i번째기호를 사용했다면 패스하기 
       if(visited[i]) continue;
-      // 방문한적없는경우
-
-      //경우의 수 세기
-      // 첫번째 자리부터 oper의 첫번째 배열부터 answer에 넣어두고 경우수 체크하기 
+      // 방문한적이없다면 -> 즉 i번쨰 기호를 사용해준적이 없다면
+      // 해당 연산기호를 answer 에 넣어준다. 
       visited[i] = true;
       answer.push(oper[i]);
-      // 재귀호출 > 
-      // 연산기호 oper의 길이만큼 경우수를 구할때까지 재귀호출.
+      console.log(answer);
+      // 재귀호출 
+      // (if문 조건에 걸릴때까지 호출하기)
+      // 재귀호출로 자기의 자식 노드를 전부 호출 탐색
       dfs(cnt+1);
-      // 모든 경우의 수를 다 찾았으면 false로 바꿔주기
+			// 더이상 자식 없다면
+      // 하나씩 빼주기 
       visited[i] = false;
-      console.log(answer)
       answer.pop();
     }
   }
 
 
-  //2. 
+  //3. 
   function calc(nums, operators) {
     //주어진 첫번째 숫자부터 연산시작
     let tmp = nums[0];
@@ -67,11 +79,12 @@ function solution(N, nums, operators) {
       // 계산해주는 함수의 인자로 첫번째값, 두번째값, 연산자 넣기
       // 그값을 다시 tmp로 저장하고 위의 과정 반복! (operators 배열의 길이만큼 반복)
       tmp = calcWorking(tmp, nums[i+1], operators[i]);
+      console.log(nums, operators,tmp);
     }
     return tmp;
   } // calc
 
-  // 3.
+  // 4.
   // 실제로 계산이 이뤄지는 함수
   // 나누기 조건 : 몫만 가져온다.(나머지는 버리기), 음수라면 절대값으로 바꿘후 나누고 다시 음수로 바꿔주기 
   function calcWorking(a, b, operator) {
@@ -96,7 +109,7 @@ function solution(N, nums, operators) {
 
 
   // 1. 
-  //입력받은 갯수를 연산자로 바꿔주는 함수
+  //입력받은 연산자의 갯수를 연산자로 바꿔주는 함수
   //[1,1,1,1] => ['+','-','*','%']
   function changeOper(operators) {
     // 연산자 기호를 넣어줄 배열
@@ -110,6 +123,8 @@ function solution(N, nums, operators) {
       else if(i ===3) opr = '%';
 
       //입력받은 연산자의 갯수만큼 빈배열 operate에 넣어주기 
+      // operators[i] => 입력받은 연산기호의 횟수
+      // [2,0,1,3]  => ['+','+','*','%','%','%']
       for(let j=0; j<operators[i]; j++) {
           operator.push(opr);
       }
--- practice.js
+++ practice.js
@@ -1,7 +1,16 @@
-const four = 'abcd'
-const data = 'apple';
-const data2 = 'fghic';
-const data3 = 'bcd';
-console.log(four.indexOf(data[0]));
-console.log(four.indexOf(data2[4]))
-console.log(four.indexOf(data3));
+let bfs = function (node) {
+  // TODO: 여기에 코드를 작성합니다.
+
+  let values = [node.value] //그 값이 들어가고
+  let queue = [node] //노드 자체가 들어간다
+
+  while(queue.length!==0){
+    let search = queue.shift(); //탐색 시작할 노드 하나 빼서
+    for(let i=0; i<search.children.length; i++){ //그 노드의 children 배열을 순회
+        values.push(search.children[i].value) //값은 values에 밀어넣고
+        queue.push(search.children[i]) //children노드는 또 탐색을 해주기 위해 queue에 넣어준다
+    }
+  }
+  return values;
+   
+};
