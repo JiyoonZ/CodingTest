d205716c9980bc1b7469f91565f30e05dd166d33
JiyoonZ <wldbsl416@gmail.com>
2023-10-23 16:01:58 +0900

    commit Test by Zeeyoon

--- baekjoon/zoac4.js
+++ baekjoon/zoac4.js
@@ -8,7 +8,6 @@ let N = input.shift();
 function countPerson(seat_count, interval_count) {
     
     if(seat_count % (interval_count+ 1) ==0 && interval_count !== 1) {
-        // 나누어 떨어질때 
         return seat_count / (interval_count+1);
     } else {
         return Math.ceil(seat_count / (interval_count+1));
